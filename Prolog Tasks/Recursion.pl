bd23190b428e85f7cda3a164a106e40b40fc20c2
MatthewTG1 <34534781+MatthewTG1@users.noreply.github.com>
2018-03-27 14:46:42 +0800

    Recursion

--- Prolog Tasks/Recursion.pl
+++ Prolog Tasks/Recursion.pl
@@ -25,9 +25,26 @@ fibonacci(A,B):-
 	B is Y+C.
 
 bunnyEars2(0,0).
-bunnyEars2(1,2).
-bunnyEars2(2,5).
-bunnyEars2(A,B):-
+bunnyEars2(A,B) :-
+	X is A-1,even(X),bunnyEars2(X,Y),B is Y+2;
+	X is A-1,odd(X),bunnyEars2(X,Y),B is Y+3.
+
+triangle(0,0).
+triangle(1,1).
+triangle(A,B) :-
+	X is A-1,triangle(X,Y),B is Y+X+1.
+
+sumDigits(0,0).
+sumDigits(A,B) :-
+	X is div(A,10),
+	sumDigits(X,Y),
+	B is Y+mod(A,10).
+
+isSeven(7).
+countSeven(0, 0).
+countSeven(A,B) :-
+	D is mod(A,10),X is div(A,10),countSeven(X,Y),
+	((isSeven(D), B is Y+1 );(not(isSeven(D)), B is Y)).
 	
 	
 	
